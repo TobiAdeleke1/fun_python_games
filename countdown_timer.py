24a059bea2e29d9433f27e049b3b2213987e5862
Tobi Adeleke <tobiaadeleke@gmail.com>
2023-02-04 21:08:13 +0000

    random countdown timer

---
+++ countdown_timer.py
@@ -0,0 +1,22 @@
+import time
+import random
+
+
+def count_down(t):
+    computer_choices = ['Go Dancing', 'Go Swimming',
+                    'Do Some-work', 'Go Eat Something',
+                    'You Chose', 'Have a beautiful day ']
+    random.shuffle(computer_choices)
+    while t:
+        mins, secs = divmod(t,60)
+        timer = '{:02d}:{:02d}'.format(mins,secs) 
+        print(timer, end='\n')
+        t -= 1
+        time.sleep(1)
+    computer_output = random.choice(computer_choices)
+    print("I command:", computer_output)
+
+
+if __name__ == '__main__':
+    user_input = input("How long do you want the counter in secs: ")
+    count_down(int(user_input))
